70ac0e986e3ad9a1fa194591c3e2d2d87f912a62
matigastirami <matigastirami@gmail.com>
2022-08-12 12:07:24 -0300

    chore: e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,51 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/tasks (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/tasks')
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        const body = res.body;
+        expect(body).toBeDefined();
+        expect(Array.isArray(body)).toBeTruthy();
+        expect(body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              uuid: expect.any(String),
+              title: expect.any(String),
+              status: expect.any(String),
+            }),
+          ]),
+        );
+      });
+  });
+
+  it('/tasks (PUT)', () => {
+    const uuid = '9994d43b-efe6-4d80-882f-6429d9ee66d2';
+    return request(app.getHttpServer())
+      .put(`/tasks/${uuid}`)
+      .expect(200)
+      .expect((res) => {
+        const body = res.body;
+        expect(body).toBeDefined();
+        expect(body).toHaveProperty('msg');
+        expect(body.msg).toBe(
+          'Task status for uuid 9994d43b-efe6-4d80-882f-6429d9ee66d2 sucessfully updated',
+        );
+      });
+  });
+
+  it('/tasks (PUT) with invalid uuid', () => {
+    const uuid = 'invalid-string';
+    return request(app.getHttpServer())
+      .put(`/tasks/${uuid}`)
+      .expect(400)
+      .expect((res) => {
+        const body = res.body;
+        expect(body).toBeDefined();
+        expect(body).toHaveProperty('message');
+        expect(body.message).toBe('Invalid UUID');
+      });
   });
 });

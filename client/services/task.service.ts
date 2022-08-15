5e05669fd18ba1bd879a7e1f5a4e42492b7bf977
matigastirami <matigastirami@gmail.com>
2022-08-15 17:19:07 -0300

    chore: update config

--- client/services/task.service.ts
+++ client/services/task.service.ts
@@ -1,7 +1,7 @@
 
 export default class TaskService {
 
-    private static readonly TASKS_API_URL = 'http://localhost:3000'
+    private static readonly TASKS_API_URL = 'http://localhost:3001'
 
     static fetchTasks(quantity?: number) {
         const url = new URL(`${TaskService.TASKS_API_URL}/tasks`);
